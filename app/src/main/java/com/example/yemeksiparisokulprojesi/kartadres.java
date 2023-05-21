355005e67962b3395c6100ca97b14e6bebd02021
murphy2811 <kubrakulak1102@gmail.com>
2023-05-22 01:37:10 +0300

    x

---
+++ app/src/main/java/com/example/yemeksiparisokulprojesi/kartadres.java
@@ -0,0 +1,14 @@
+package com.example.yemeksiparisokulprojesi;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class kartadres extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kartadres);
+    }
+}
